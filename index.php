fa0ccd0c60fbca11ba8afb24375ce240dc64cf93
Александр <32023344+AlexandrRumiantsev@users.noreply.github.com>
2017-11-26 00:40:12 +0300

    fixBag

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ if (mysqli_multi_query($link, $query)) {
 <button data-msg_time='$row[8]' data-msg_id='$row[1]' data-user_id='$row[0]' data-louder_id='louder$row[0]$row[1]' class='agaxClose' id='agaxClose' style='background-image: url(images/crestic.png);'>
 </button>";
                 echo"
-<button data-msg_time='$row[8]' data-msg_id='$row[1]' data-user_id='$row[0]' data-louder_id='louder$row[0]$row[8]' class='agaxEdit' id='agaxEdit' style='background-image: url(images/edit.png);'>
+<button data-msg_time='$row[8]' data-msg_id='$row[1]' data-user_id='$row[0]' data-louder_id='louder$row[0]$row[1]' class='agaxEdit' id='agaxEdit' style='background-image: url(images/edit.png);'>
 </button>";
 				echo"<br><br><div class='blockOne'> $expd[2].$expd[1].$expd[0]</div><br><br><br><br>";
                 echo"<div id='louder$row[0]$row[1]' class='louderClass'><img src='images/loader.gif' /></div>";
