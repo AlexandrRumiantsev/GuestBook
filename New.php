88a1c066b5a514b4fb8f701fa59ca791e156a540
Александр <32023344+AlexandrRumiantsev@users.noreply.github.com>
2017-11-27 22:53:15 +0300

    реф

--- New.php
+++ New.php
@@ -5,37 +5,25 @@ $name = $_POST['msg_from'];
 $mail = $_POST['msg_mail'];
 $url = $_POST['msg_url'];
 $time = time();
-
 //Работа с картинкой
 $file = $_FILES['filesName'];
-
 if($text == null or $name == null){echo "<script> alert('Необходимо авторизоватся и заполнить поле - Сообщение'); window.location.href = 'index.php';</script>";} 
 $nameFile = $_FILES['filesName']['name'];
 //Определяем IP и Браузер пользователя
 $Br = $_SERVER['HTTP_USER_AGENT'];
 $ip = $_SERVER['REMOTE_ADDR'];
-
 $type = $_FILES['filesName']['type'];
 $size = $_FILES['filesName']['size'];
+//Проверка файла на вес и тип
 $audit = new audit();
 $resultAuditSize = $audit ->auditSize($size);
 $resultAuditType = $audit ->auditType($type);
-//Проверка файла на РАЗМЕР не более 1мб
-
-
-//Проверка текстового файла на ТИП
-if($_FILES['filesName']['type'] == 'text/plain')
-{echo "Ввод текстового файла на данный момент не реализован!";};
-
 //Запись файла в папку
 $file = $_FILES["filesName"]["name"];
 $fileTmp =$_FILES["filesName"]["tmp_name"];
-
 $fileToDirect = new fileToDirect();
 $mCode = $fileToDirect->save_source_code($file,$fileTmp);
-
 $pathPicture = $path . $_FILES['filesName']['names'];
-
 $text = strip_tags($text);
 $name = strip_tags($name);
 $comment =   '<div style="font-weight:bolder; align:center;">'  .'</div>'  .'<br>'  .'<p style="width:340px;">' .$text .'</p>' .'<hr style="display:block; width:200px;">';       
