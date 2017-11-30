a80b2095bc9a898a95344d1bee09e0b64ed9c1bf
Александр <32023344+AlexandrRumiantsev@users.noreply.github.com>
2017-11-30 23:39:15 +0300

    upp

--- index.php
+++ index.php
@@ -4,19 +4,13 @@
 // Автор: Александр Румянцев            //
 //--------------------------------------//
 ?>
-<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
-<html><head>
-<? include 'cookie.inc.php' ?>
-<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
-<title>GuestBook</title>
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="expires" content="0">
-<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script src="js/slide.js" type="text/javascript"></script>
-<script src="js/ClockAnAuto.js" type="text/javascript"></script>
-<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="css/slide.css" type="text/css" media="screen" />
+
+<html>
+<head>
+<?
+include 'cookie.inc.php';
+include 'setting.php';
+?>
 </head>
 <body style="background: url(Background.jpg)" >
 <div>
@@ -85,7 +79,8 @@ $pagination ->paginationEnter($count_pages, $active, $count_show_pages, $url,$ur
 * Имя:
 </div>
 <div align="left">
-<input type="text" class="focus" style="border-style: solid;border-radius: 7px 7px 7px 7px;" id="mess" name="msg_from" value="<?global $Login;echo $Login; ?>" placeholder="<?if($Login==Null){echo"Авторезуйтесь";}?>"maxlength="40" size="20">
+<input type="text" class="focus" style="border-style: solid;border-radius: 7px 7px 7px 7px;" id="mess" name="msg_from" value="<?global $Login;echo $Login; ?>"
+       placeholder="<?if($Login==Null){echo"Авторезуйтесь";}?>"maxlength="40" size="20">
 </div>
 </div>
 </div>
@@ -96,7 +91,8 @@ $pagination ->paginationEnter($count_pages, $active, $count_show_pages, $url,$ur
 E-Mail:
 </div>
 <div align="left">
-<input type="text"  class="focus" style="border-style: solid; border-radius: 7px 7px 7px 7px;" value="<?echo $mail;?>" placeholder="<?if($mail==Null){echo"Авторезуйтесь";}?>" name="msg_mail" maxlength="40" style="margin-right:50px;" size="20">
+<input type="text"  class="focus" style="border-style: solid; border-radius: 7px 7px 7px 7px;" value="<?echo $mail;?>" placeholder="<?if($mail==Null){echo"Авторезуйтесь";}?>"
+       name="msg_mail" maxlength="40" style="margin-right:50px;" size="20">
 </div>
 </div>
 </div>
---
+++ setting.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+<title>GuestBook</title>
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="expires" content="0">
+<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="js/slide.js" type="text/javascript"></script>
+<script src="js/ClockAnAuto.js" type="text/javascript"></script>
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/slide.css" type="text/css" media="screen" />
