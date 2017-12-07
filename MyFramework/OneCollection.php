979f8777117892cadd4b2588b20dbc6538d1b1b0
Александр <32023344+AlexandrRumiantsev@users.noreply.github.com>
2017-12-07 21:08:10 +0300

    рефакторинг

--- MyFramework/OneCollection.php
+++ MyFramework/OneCollection.php
@@ -370,6 +370,57 @@ class viewMessage
         }
     }
 }
+
+class autorUser
+{
+    function auto($Login,$Password){
+        if($Login){
+            //Авторизация
+            $link = mysqli_connect("localhost", "root", "", "GuestBook");
+            $q = "SELECT * FROM `RegUsers`";
+            //Вывод кирилицы
+            mysqli_query($link, "SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
+            mysqli_query($link, "SET CHARACTER SET 'utf8'");
+            if (mysqli_multi_query($link, $q)) {
+                do {
+                    /* получаем первый результирующий набор */
+                    if ($result = mysqli_store_result($link)) {
+                        while ($row = mysqli_fetch_row($result)) {
+                            $log = $row[0];
+                            $Pas = $row[1];
+                            //echo("<script>alert('Вы ввели неправильный логин или пароль!');document.location.replace('index.php');</script>");
+                            if ($Login == $log and $Password == $Pas) {
+                                $resultAutoriz = true;
+                                printf("<div style='background:black;  border-radius: 7px 7px 7px 7px; float:left; color:white; width:250px; height:100px; border-style:groove; position:absolute; font-size:18px;'> 
+                           <img style='width:90px; height:90px; padding-left:9px; padding-right:11px; padding-top:7px; margin-left:0px; float:left; border-radius: 75px 75px 75px 75px;' src='%s' > 
+         				   <form action='Profile.php?status=$row[7]&mail=$row[2]&town=$row[3]&pol=$row[4]&years=$row[5]&pic=$row[6]' method='post'>  <input  value='%s' type='text' class='text' style='background-color:black; color:white; padding-left:19px;  margin-top: 12px;  margin-bottom: 7px;  border-radius: 7px 7px 7px 7px; ' name='login' maxlength='20' size='10'>  <form> 
+         				   <br>
+         				   <div style='padding-left:44px;display:block;'>
+						 <input type='submit' style='background-size:contain;background-image: url(edit.png);width:35px; height:35px; border-radius: 7px 7px 7px 7px;' class='subm' name='submitUser' value=''/>
+						 <a target='_blank' href='ViewMessage.php?log=$row[0]&mail=$row[2]&town=$row[3]'> <div style='width:48px;height:35px; display: inline-block;  background-image:url(onebit_42.png); margin-top:6px; border-radius: 10px 10px 10px 10px;'> <div style='font-size: 30px; padding-left:15px; padding-top:16px;'> </div></div></a> 
+						 <a href='index.php'> <img style='width:35px;height:35px;padding-top:10px;' src='images/close.png'> </a> 
+						</div>
+						 </div> ", $row[6], $row[0]);
+                                $Login = $row[0];
+                                $mail = $row[2];
+                                require_once 'MyFramework\OneCollection.php';
+                                $library = new Library();
+                                $countMess = $library->Timer($Login);
+                                echo "<div style='color:white; position:absolute; padding-top:120px;'>У вас $countMess сообщений</div>";
+                            }else{}
+                        }if($resultAutoriz!==true){echo"<script>alert('Вы ввели неправильный логин или пароль!');</script>";}
+                        mysqli_free_result($result);
+                    }
+                    //else { echo 'Ничего не найдено';}
+                    /* печатаем разделитель */
+                    if (mysqli_more_results($link)) {
+                        printf('-----------------\n');
+                    }
+                } while (mysqli_next_result($link));
+            }
+        }
+    }
+}
 /**
  * Краткое описание класса
  * тут храню все запросы, у всех тип protected
